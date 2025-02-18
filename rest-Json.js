46d1ed068bb55f0e659533d571a911de06f35064
IntLuisa <luisa.interaminense@gmail.com>
2025-02-17 21:40:55 -0300

    tentando criar um json para restaurantes

---
+++ rest-Json.js
@@ -0,0 +1,11 @@
+
+let restaurantes = {
+    "name": "bar do bode",
+    "endereco": {
+        "rua": "rua 1",
+        "numero": 123
+    },
+    "classificacao": "3 estrelas",
+    "telefones": ["1234-5678", "8765-4321"],
+    // "logo": 
+}
